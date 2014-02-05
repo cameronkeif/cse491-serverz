6382c76d78aec151a5633f96034fda29cb5cb43d
cam <cam@debian.localdomain>
2014-02-05 08:39:12 -0500

    Handle both application/x-www-form-urlencoded and multipart/form-data submission types for POST

--- server.py
+++ server.py
@@ -4,7 +4,9 @@
 import random
 import socket
 import time
-import urlparse 
+import urlparse
+import cgi
+from StringIO import StringIO
 
 def main():
     s = socket.socket()         # Create a socket object
@@ -41,11 +43,17 @@ def handle_connection(conn):
     path = "/404"
 
   if http_method == 'POST':
+    headers_dict, content = parse_post_request(request)
+    environ = {}
+    environ['REQUEST_METHOD'] = 'POST'
+
+    form = cgi.FieldStorage(headers = headers_dict, fp = content, environ = environ)
+
     if path == '/':
       handle_index(conn, '')
     elif path == '/submit':
         # POST has the submitted params at the end of the content body
-        handle_submit(conn,request.split('\r\n')[-1])
+        handle_submit_post(conn,form)
     else:
         notfound(conn,'')
   else:
@@ -61,7 +69,7 @@ def handle_connection(conn):
           handle_image(conn,'')
       elif path == '/submit':
           # GET has the params in the URL.
-          handle_submit(conn,parsed_url[4])
+          handle_submit_get(conn,parsed_url[4])
       else:
           notfound(conn,'')
   conn.close()
@@ -77,23 +85,57 @@ def handle_index(conn, params):
             "<p>last name: <input type='text' name='lastname'></p>\n" + \
             "<p><input type='submit' value='Submit'>\n\n" + \
             "</form></p>" + \
+            "<p><u>Form Submission via POST (multipart/form-data)</u></p>"
+            "<form action='/submit' method='POST' enctype='multipart/form-data'>\n" + \
+            "<p>first name: <input type='text' name='firstname'></p>\n" + \
+            "<p>last name: <input type='text' name='lastname'></p>\n" + \
+            "<p><input type='submit' value='Submit'>\n\n" + \
+            "</form></p>" + \
             "<p><u>Form Submission via POST</u></p>"
-            "<form action='/submit' method='POST'>\n" + \
+            "<form action='/submit' method='POST'\n" + \
             "<p>first name: <input type='text' name='firstname'></p>\n" + \
             "<p>last name: <input type='text' name='lastname'></p>\n" + \
             "<p><input type='submit' value='Submit'>\n\n" + \
             "</form></p>")
 
-def handle_submit(conn, params):
+def handle_submit_post(conn, form):
     ''' Handle a connection given path /submit '''
     # submit needs to know about the query field, so more
     # work needs to be done here.
-    
-    params = urlparse.parse_qs(params)
 
     # we want the first element of the returned list
-    firstname = params['firstname'][0]
-    lastname = params['lastname'][0]
+
+    try:
+      firstname = form['firstname'].value
+    except KeyError:
+      firstname = ''
+    try:
+      lastname = form['lastname'].value
+    except KeyError:
+      lastname = ''
+
+    # Screw the patriarchy! Why's it gotta be "Mr."?! - @CTB, hah!
+    conn.send('HTTP/1.0 200 OK\r\n' + \
+              'Content-type: text/html\r\n' + \
+              '\r\n' + \
+              "Hello Mrs. %s %s." % (firstname, lastname))
+
+def handle_submit_get(conn, params):
+    ''' Handle a connection given path /submit '''
+    # submit needs to know about the query field, so more
+    # work needs to be done here.
+
+    # we want the first element of the returned list
+    params = urlparse.parse_qs(params)
+
+    try:
+      firstname = params['firstname'][0]
+    except KeyError:
+      firstname = ''
+    try:
+      lastname = params['lastname'][0]
+    except KeyError:
+      lastname = ''
 
     # Screw the patriarchy! Why's it gotta be "Mr."?! - @CTB, hah!
     conn.send('HTTP/1.0 200 OK\r\n' + \
@@ -130,6 +172,33 @@ def notfound(conn, params):
             'Content-type: text/html\r\n' + \
             '\r\n' + \
             'Oopsies, this isn\'t the page you want. :(')
+
+def parse_post_request(request):
+  ''' Takes in a request (as a string), parses it, and
+      returns a dictionary of header name => header value 
+      returns a StringIO object built from the content of the request
+      Sidenote: God I love that you can do this in Python. Python is great.'''
+  header_dict = dict()
+
+  request = request.split('\r\n')
+
+  # Headers are separated from the content by '\r\n'
+  # which, after the split, is just ''.
+  separation_line_index = request.index('')
+
+  # First line isn't a header, but everything else
+  # up to the empty line is. The names are separated
+  # from the values by ': '
+  for i in range(1,separation_line_index):
+    header = request[i].split(': ', 1)
+    header_dict[header[0].lower()] = header[1]
+
+  # Originally separated by '\r\n' so make sure we put it back in, in case
+  # there are multiple lines!
+  
+  content = StringIO('\r\n'.join(request[separation_line_index + 1:]))
+
+  return header_dict, content
 
 if __name__ == '__main__':
    main()
