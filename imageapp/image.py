ed9826e4f4493cc56a3e10ca4b6993ee322eb201
cam <cam@debian.localdomain>
2014-04-09 12:57:42 -0400

    Missing things

--- imageapp/image.py
+++ imageapp/image.py
@@ -1,4 +1,6 @@
 # image handling API
+import sqlite3
+import sys
 
 class Image:
     filename = ''
@@ -25,11 +27,54 @@ def add_image(filename, data):
 
     image = Image(filename, data)
     images[image_num] = image
+
+    # insert to database
+    insert_image(filename, data)
+
     return image_num
 
 def get_image(num):
-    return images[num]
+    return retrieve_image(num)
 
 def get_latest_image():
-    image_num = max(images.keys())
-    return images[image_num]
+    return retrieve_image(-1)
+
+def insert_image(filename, data):
+    # connect to the already existing database
+    db = sqlite3.connect('images.sqlite')
+
+    # configure to allow binary insertions
+    db.text_factory = bytes
+
+    # grab whatever it is you want to put in the database
+
+    # insert!
+    db.execute('INSERT INTO image_store (filename, image) VALUES (?,?)', (filename, data))
+    db.commit()
+
+# retrieve an image from the database.
+def retrieve_image(i = -1):
+    # connect to database
+    db = sqlite3.connect('images.sqlite')
+    
+    # configure to retrieve bytes, not text
+    db.text_factory = bytes
+
+    # get a query handle (or "cursor")
+    c = db.cursor()
+
+    # select all of the images
+    if i >= 0:
+        c.execute('SELECT i, filename, image FROM image_store where i=(?)', i)
+    else:
+        c.execute('SELECT i, filename, image FROM image_store ORDER BY i DESC LIMIT 1')
+
+    # grab the first result (this will fail if no results!)
+    try:
+        i, filename, image = c.fetchone()
+        print image
+        print filename
+
+        return Image(filename, image)
+    except:
+        pass
